c5aecd2095afb7a651a0be2ae1e4f8ef112bf99b
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-07-09 08:35:23 +0200

    feat(HistoricalDataChart): Add default date values to search form

--- aquaponics-app/src/components/HistoricData/HistoricalData.jsx
+++ aquaponics-app/src/components/HistoricData/HistoricalData.jsx
@@ -23,10 +23,13 @@ ChartJS.register(
 );
 
 const HistoricalDataChart = ({ moduleId }) => {
-  const [historicalData, setHistoricalData] = useState([]);
-  const [start, setStart] = useState("");
-  const [stop, setStop] = useState("");
-  const [mode, setMode] = useState("hourly");
+    const defaultStart = new Date('2024-07-07T12:00:00.000Z').toISOString().slice(0, 16);
+    const defaultStop = new Date('2024-07-08T12:00:00.000Z').toISOString().slice(0, 16);
+  
+    const [historicalData, setHistoricalData] = useState([]);
+    const [start, setStart] = useState(defaultStart);
+    const [stop, setStop] = useState(defaultStop);
+    const [mode, setMode] = useState('hourly');
 
   useEffect(() => {
     if (start && stop && mode) {
@@ -49,7 +52,7 @@ const HistoricalDataChart = ({ moduleId }) => {
         label: "Temperature",
         data: historicalData.map((entry) => entry.temperature),
         fill: false,
-        borderColor: "rgb(75, 192, 192)",
+        borderColor: "#00b7c2",
         tension: 0.1,
       },
     ],

9bd48328e46d8f0dc9bba3024cc4e5b35500703d
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-07-10 12:13:49 +0200

    refactor : implement redux  refactor

--- aquaponics-app/src/components/ModuleList/ModuleList.jsx
+++ aquaponics-app/src/components/ModuleList/ModuleList.jsx
@@ -1,44 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchModules } from "../../redux/moduleSlice";
+import { fetchModules } from "../../redux/thunks/module.thunk";
+import TemperatureProvider from "../../redux/Temperature/temperatureProvider";
+
 import ModuleListItem from "./ModuleListItem";
-import socket from "../../utils/socket";
 
 import { Modulediv, ModuleElemet } from "./ModuleList.styled";
 
 const ModuleList = () => {
   const dispatch = useDispatch();
   const modules = useSelector((state) => state.modules.modules);
-  const [currentTemperatures, setCurrentTemperatures] = useState({});
 
   useEffect(() => {
     dispatch(fetchModules());
-
-    socket.on("temperatureUpdate", (data) => {
-      setCurrentTemperatures((prevTemps) => ({
-        ...prevTemps,
-        [data.id]: data.temperature,
-      }));
-    });
-
-    return () => {
-      socket.off("temperatureUpdate");
-    };
   }, [dispatch]);
 
   return (
-    <Modulediv>
-
-      <ModuleElemet>
-        {modules.map((module) => (
-          <ModuleListItem
-            key={module.id}
-            module={module}
-            currentTemperature={currentTemperatures[module.id]}
-          />
-        ))}
-      </ModuleElemet>
-    </Modulediv>
+    <TemperatureProvider>
+      <Modulediv>
+        <ModuleElemet>
+          {modules.map((module) => (
+            <ModuleListItem key={module.id} module={module} />
+          ))}
+        </ModuleElemet>
+      </Modulediv>
+    </TemperatureProvider>
   );
 };
 
